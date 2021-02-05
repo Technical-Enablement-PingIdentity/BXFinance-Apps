8699bbd08e41031a787fcff2afb0d62f21d544da
Michael Sanchez <michaelsanchez@pingidentity.com>
2021-02-05 20:48:07 +0000

    Modified error modal to have Try Again and Quit options for login. https://www.pivotaltracker.com/story/show/176600842

--- bxfinance-scenario1/src/components/ModalError/ModalError.js
+++ bxfinance-scenario1/src/components/ModalError/ModalError.js
@@ -1,7 +1,7 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 
 // Packages
-import React from 'react';
+import React from "react";
 import {
   Button,
   Modal,
@@ -11,17 +11,18 @@ import {
   Label,
   Input,
   CustomInput,
-  TabContent, TabPane
-} from 'reactstrap';
-import { Redirect } from 'react-router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+  TabContent,
+  TabPane,
+} from "reactstrap";
+import { Redirect } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 // Styles
 import "./ModalError.scss";
 
 // Data
-import data from './data.json';
+import data from "./data.json";
 
 class ModalError extends React.Component {
   constructor() {
@@ -29,36 +30,41 @@ class ModalError extends React.Component {
     this.state = {
       isOpen: false,
       errorTitle: "Unexpected Error.",
-      errorMsg: "Please contact Technical Enablement.",
-      callBack: ""
+      errorMsg: "Please contact Ping Identity Technical Enablement.",
     };
     this.toggle = this.toggle.bind(this);
+    this.startSSOURI = "/idp/startSSO.ping?PartnerSpId=" + process.env.REACT_APP_HOST; //TODO this needs to be moved to ping-endpoints.json
   }
   toggle(title, msg, callBack) {
     this.setState({
       isOpen: !this.state.isOpen,
       errorTitle: title,
       errorMsg: msg,
-      callBack: callBack
+      callBack: callBack,
     });
   }
   continueBtn() {
-    if (typeof this.state.callBack === 'function') {
-      this.state.callBack();
-    } else {
-      window.location.assign("/app/");
-    }
+    window.location.assign(this.startSSOURI);
   }
   render() {
     const closeBtn = <div />;
     return (
       <div>
-        <Modal isOpen={this.state.isOpen} toggle={this.toggle.bind(this)} className="modal-error">
-          <ModalHeader toggle={this.toggle.bind(this)} close={closeBtn}><img src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="logo" /></ModalHeader>
+        <Modal
+          isOpen={this.state.isOpen}
+          toggle={this.toggle.bind(this)}
+          className="modal-error"
+        >
+          <ModalHeader toggle={this.toggle.bind(this)} close={closeBtn}>
+            <img src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="logo" />
+          </ModalHeader>
           <ModalBody>
             <h4>{this.state.errorTitle}</h4>
             <div>{this.state.errorMsg}</div>
-            <Button color="primary" onClick={this.continueBtn.bind(this)}>{data.button}</Button>
+            <Button color="primary" onClick={this.continueBtn.bind(this)}>
+              {data.button}
+            </Button><br />
+            <Button type="button" color="link" size="sm" className="text-info pl-0" onClick={() => { this.toggle(); }}>Quit</Button> {/* PING INTEGRATION:*/}
           </ModalBody>
         </Modal>
       </div>
