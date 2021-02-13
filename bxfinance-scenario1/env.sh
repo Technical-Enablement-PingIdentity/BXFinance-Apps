8e97c48a5acc016b5266c374b99edec1ffe1eebf
Michael Sanchez <michaelsanchez@pingidentity.com>
2021-02-13 01:15:15 +0000

    Changes for new prod image using runtime vars solution. This is not complete as it works but its not accepting vars injected from the container. Uses only defaults. https://www.pivotaltracker.com/story/show/176859598

--- bxfinance-scenario1/env.sh
+++ bxfinance-scenario1/env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Recreate config file
 rm -rf ./env-config.js
@@ -9,18 +9,21 @@ echo "window._env_ = {" >> ./env-config.js
 
 # Read each line in .env file
 # Each line represents key=value pairs
-while read -r line || [[ -n "$line" ]];
+#TODO need to only read lines that start REACT_APP_
+while read -r line || test -n "${line}"
 do
   # Split env variables by character `=`
-  if printf '%s\n' "$line" | grep -q -e '='; then
-    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+  if printf '%s\n' "${line}" | grep -q -e '='
+  then
+    varname=$(printf '%s\n' "${line}" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "${line}" | sed -e 's/^[^=]*=//')
   fi
 
   # Read value of current variable if exists as Environment variable
   value=$(printf '%s\n' "${!varname}")
   # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
+  #TODO need to skip lines that are comments
+  test -z ${value} && value=${varvalue}
   
   # Append configuration property to JS file
   echo "  $varname: \"$value\"," >> ./env-config.js
