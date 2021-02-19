8067eb216f80587f4965ec72a96dadc7e426aeac
Michael Sanchez <michaelsanchez@pingidentity.com>
2021-02-19 01:06:10 +0000

    Made it so Chat With Us form opens in new tab in advisor footer.

--- bxfinance-scenario1/src/pages/any-wealth-advisor.js
+++ bxfinance-scenario1/src/pages/any-wealth-advisor.js
@@ -222,7 +222,7 @@ class AnyWealthAdvisor extends React.Component {
                           {item.children.map((item, i) => {
                             return (
                               <NavItem key={i}>
-                                <NavLink href={item.url}>{item.title}</NavLink>
+                                <NavLink target="_blank" href={item.url}>{item.title}</NavLink>
                               </NavItem>
                             );
                           })}
