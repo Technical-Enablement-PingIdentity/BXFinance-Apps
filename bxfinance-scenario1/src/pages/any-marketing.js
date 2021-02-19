d7125824b3dc774df3bab9e1094ded176f36347c
Michael Sanchez <michaelsanchez@pingidentity.com>
2021-02-19 01:00:02 +0000

    Updated Chat with Us Google Form to open in new tab on Marketing site.

--- bxfinance-scenario1/src/pages/any-marketing.js
+++ bxfinance-scenario1/src/pages/any-marketing.js
@@ -409,7 +409,7 @@ class AnyMarketing extends React.Component {
                           {item.children.map((item, i) => {
                             return (
                               <NavItem key={i}>
-                                <NavLink href={item.url}>{item.title}</NavLink>
+                                <NavLink target = "_blank" href={item.url}>{item.title}</NavLink>
                               </NavItem>
                             );
                           })}
